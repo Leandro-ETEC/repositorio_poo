b125ff0c1cd0687b0f21958a6ce29295488c4345
Leandro-ETEC <107407398+Leandro-ETEC@users.noreply.github.com>
2022-11-09 19:45:44 -0300

    Adicionado 2 classes.
    Incluído as classes: Principal e Clientes.

---
+++ projeto-banco/src/br/com/leandro/banco/Clientes.java
@@ -0,0 +1,5 @@
+package br.com.leandro.banco;
+
+public class Clientes {
+
+}
---
+++ projeto-banco/src/br/com/leandro/banco/Principal.java
@@ -0,0 +1,10 @@
+package br.com.leandro.banco;
+
+public class Principal {
+
+	public static void main(String[] args) {
+
+
+	}
+
+}
